55f4fe2afca5aa12b188518403f7dec5a8b00e1e
The5-1 <agaku@hotmail.de>
2017-12-19 18:42:20 +0100

    Entity Component System: OOP and DOD coposition

--- CPP_test/ECS_OOP_vs_DOD.h
+++ CPP_test/ECS_OOP_vs_DOD.h
@@ -5,6 +5,12 @@
 //compares different approaches with sample code
 https://github.com/CppCon/CppCon2015/blob/master/Tutorials/Implementation%20of%20a%20component-based%20entity%20system%20in%20modern%20C%2B%2B/Implementation%20of%20a%20component-based%20entity%20system%20in%20modern%20C%2B%2B%20-%20Vittorio%20Romeo%20-%20CppCon%202015.pdf
 
+Video with live coding!!!
+https://www.youtube.com/watch?v=NTWSeQtHZ9M
+
+Code:
+https://github.com/CppCon/CppCon2015/tree/master/Tutorials/Implementation%20of%20a%20component-based%20entity%20system%20in%20modern%20C%2B%2B/Source%20Code
+
 ******************************************/
 
 #include <memory>
@@ -18,7 +24,7 @@ https://github.com/CppCon/CppCon2015/blob/master/Tutorials/Implementation%20of%2
 //-------------------
 //++ flexible
 //-- cache unfriendly
-//-- runtime overhead
+//-- runtime overhead due to runtime Polymorphism
 namespace ECS_OOP_composition
 {
 	struct Component
@@ -65,15 +71,29 @@ namespace ECS_OOP_composition
 }
 
 //===================
-//= DOD composition =
+//= DOD composition = (Data-Oriented-Design = cache friendly)
 //===================
 // Components only store data
 // Systems handle logic
 //-------------------
 //++ flexible
 //~~ can be more cache friendly if done right
-//++ minimal runtime overhead
+//++ minimal runtime overhead due to compile-time Polymorphism
 namespace ECS_DOD_composition
 {
+	using Entity = std::size_t;
+	constexpr std::size_t maxEntities{ 1000 };
 
+	struct BonesComponent {/*...*/ };
+	struct AIComponent {/*...*/ };
+	struct SpriteComponent {/*...*/ };
+	
+	struct Manager
+	{
+		std::array<BonesComponent, maxEntities> bonesComponents;
+		std::array<AIComponent, maxEntities> aiComponents;
+		std::array<SpriteComponent, maxEntities> spriteComponents;
+
+	};
+	
 }
