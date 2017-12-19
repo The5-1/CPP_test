f94e97a6b2e5c8fd5d67317cd6a1fee26baf88ec
The5-1 <agaku@hotmail.de>
2017-12-19 16:11:45 +0100

    pointer vs reference

---
+++ CPP_test/exercise_pointer_VS_reference.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+/*
+References are syntatcic sugar for pointers!
+
+
+//https://stackoverflow.com/questions/57483/what-are-the-differences-between-a-pointer-variable-and-a-reference-variable-in?page=1&tab=votes#tab-top
+
+
+
+//http://yosefk.com/c++fqa/ref.html
+
+References: use in function parameters and return types to define useful and self-documenting interfaces
+- so you can directly pass the "object" rather than a copy or a pointer
+- cant be nullptr and must have a value at compile time
+- use them like a value (they are an alias for a an object)
+
+Pointers: use to implement algorithms and data structures
+- pointers may be nullprt  
+- refrences must have a value at compile time
+- pointers can be re-assigned and adresses manipulated
+
+
+*/
