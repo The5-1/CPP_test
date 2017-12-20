31be20180dcdec3d136a4a53413661e930a05887
The5-1 <agaku@hotmail.de>
2017-12-20 20:20:37 +0100

    inheritance & polymorphism

---
+++ CPP_test/exercise_inheritance_polymorphism.h
@@ -0,0 +1,21 @@
+#pragma once
+namespace exercise_inheritance_polymorphism
+{
+	//https://stackoverflow.com/questions/1563765/use-of-public-in-a-derived-class-declaration
+
+	class B { };
+
+	class D1 : B { }; //all members of B are turned to private, casting is not possible -> "D1 makes use of B"
+
+	class D2 : public B { }; //all members of B remain -> "D2 is a B"
+
+	void foo(B* baseArgument) {};
+
+
+	void test()
+	{
+		foo(new D1); //conversion to inacessible base class
+		foo(new D2); //this works
+	}
+
+}
